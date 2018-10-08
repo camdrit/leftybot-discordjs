5d6f2c4e99e5fd0271e1d218197c3b4c19ceb880
Cam Richardson <cameronrichardsonit@gmail.com>
2018-10-08 11:04:00 -0400

    Add command structure

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
+const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 
 client.on('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
+	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong.');
 	}
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(token);
